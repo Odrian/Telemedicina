c05b47ea72d54ee013fa04472121d0992228b9f4
Adrian <adrian46tver@bk.ru>
2020-11-25 20:44:25 +0300

    add Cryption class

---
+++ src/Cryption.java
@@ -0,0 +1,11 @@
+public class Cryption {
+    public static String UnCryption(String str){ // Разшивровщик
+
+        return "";
+    }
+
+    public static String Cryption(String str){ // Шифровальшик
+        
+        return "";
+    }
+}
